2d315a7a2283600424d99aaa2ebbe2af2684d6e8
Mikkel Vester <24669821+UranVester@users.noreply.github.com>
2026-09-02 19:22:36 +0200

    v1.4

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 if (!defined('LANEASSIST_MODULE_VERSION')) {
     // Bump this version on every release.
-    define('LANEASSIST_MODULE_VERSION', '1.3');
+    define('LANEASSIST_MODULE_VERSION', '1.4');
 }
 
 if (!function_exists('getLaneAssistModuleVersion')) {
